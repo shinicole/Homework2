7ab32a42a6769af7bf69c9367322933bc3edd9cd
Shivon Talavera <t.shivon@yahoo.com>
2023-01-22 19:24:25 -0600

    Week Two Homework

---
+++ Week2HW/PizzaHomework.js
@@ -0,0 +1,21 @@
+let pizzaPlace = "Antonio's Yummy Pizza";
+let numberOfToppings = "11";
+
+console.log(pizzaPlace);
+console.log ("numberOfToppings");
+console.log (typeof pizzaPlace); //string
+console.log(typeof numberOfToppings); //string
+
+let str = `${pizzaPlace} had ${numberOfToppings} different pizza toppings`
+console.log(str);
+
+if (numberOfToppings < 20) {
+  console.log("Quality, not quantity");
+}
+else if (numberOfToppings > 10) {
+  console.log("A whole lot of pizza");
+}
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log (i);
+  }
---
+++ Week2HW/README.md
@@ -0,0 +1,10 @@
+# Shivon Talavera
+
+## About Me
+## My name is Shivon and I'm from New York. I love to travel, learn new things and go shopping.
+
+### My goals for this course:
+ 1. To learn and understand how to code
+ 2. To challenge myself
+ 3. To successfully complete my capstone project
+ 4. To be able to transition my career to web development
